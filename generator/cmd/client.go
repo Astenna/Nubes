8d0a291d80c936bf29ee7b0e240889bc1255fb47
Kinga Marek <marekkinga97@gmail.com>
2022-12-16 15:11:57 +0100

    generator: WIP types for client project generator

--- generator/cmd/client.go
+++ generator/cmd/client.go
@@ -1,8 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/Astenna/Thesis_PoC/generator/parser"
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra-cli/cmd"
 )
@@ -13,11 +12,42 @@ var clientCmd = &cobra.Command{
 	Long:  `Generates client project based on types and repositories.`,
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("client called!")
+		typesPath, _ := cmd.Flags().GetString("types")
+		repositoriesPath, _ := cmd.Flags().GetString("repositories")
+		//output, _ := cmd.Flags().GetString("output")
+		_ = repositoriesPath
+
+		parser.PrepareTypesFiles(MakePathAbosoluteOrExitOnError(typesPath))
+		//types := parser.PrepareTypes(MakePathAbosoluteOrExitOnError(typesPath))
+		//templ, _ := template.ParseFiles("type_template.go.tmpl")
+
+		// outputDirectoryPath := MakePathAbosoluteOrExitOnError(filepath.Join(output, "output_testing"))
+		// os.MkdirAll(outputDirectoryPath, 0777)
+
+		// for i, f := range types {
+		// 	file, err := os.Create(filepath.Join(outputDirectoryPath, "test"+strconv.Itoa(i)+".go"))
+		// 	if err != nil {
+		// 		fmt.Println(err)
+		// 	}
+		// 	err = templ.Execute(file, f)
+		// 	if err != nil {
+		// 		fmt.Println(err)
+		// 	}
+		// 	defer file.Close()
+		// }
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(clientCmd)
+
+	var typesPath string
+	var repositoriesPath string
+	var outputPath string
+
+	clientCmd.Flags().StringVarP(&typesPath, "types", "t", ".", "path to directory with types")
+	clientCmd.Flags().StringVarP(&repositoriesPath, "repositories", "r", ".", "path to directory with repositories")
+	clientCmd.Flags().StringVarP(&outputPath, "output", "o", ".", "path where directory with client project will be created")
+
 	cmd.Execute()
 }
