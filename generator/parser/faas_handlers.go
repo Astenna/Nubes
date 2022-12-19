86b57b7f8b813e02b23de252746f1ed8d498273e
Kinga Marek <marekkinga97@gmail.com>
2022-12-19 21:07:58 +0100

    generator: fix imports in generated handlers

--- generator/parser/faas_handlers.go
+++ generator/parser/faas_handlers.go
@@ -41,12 +41,18 @@ type RepositoryCustomHandlerFunc struct {
 	Body          string
 }
 
+type detectedFunction struct {
+	Function    *ast.FuncDecl
+	PackageName string
+	Imports     []*ast.ImportSpec
+}
+
 func PrepareRepositoriesHandlers(path string, moduleName string, nobjectTypes map[string]struct{}) []RepositoryCustomHandlerFunc {
 	set := token.NewFileSet()
 	packs, err := parser.ParseDir(set, path, nil, 0)
 	AssertDirParsed(err)
 
-	funcsMap := GetPackageFunctionMap(packs)
+	functions := GetPackageFunctions(packs)
 
 	isNoObjectMethodDefined := make(map[string]map[string]bool, len(nobjectTypes))
 	for i := range nobjectTypes {
@@ -54,54 +60,51 @@ func PrepareRepositoriesHandlers(path string, moduleName string, nobjectTypes ma
 	}
 
 	repositoryHandlerFuncs := []RepositoryCustomHandlerFunc{}
-	for packageName, funcs := range funcsMap {
-		for _, f := range funcs {
+	for _, detectedFunction := range functions {
+		f := detectedFunction.Function
+		var methodType string
 
-			var methodType string
+		switch {
+		case strings.HasPrefix(f.Name.Name, GetPrefix):
+			methodType = GetPrefix
+		case strings.HasPrefix(f.Name.Name, CreatePrefix):
+			methodType = CreatePrefix
+		case strings.HasPrefix(f.Name.Name, DeletePrefix):
+			methodType = DeletePrefix
+		default:
+			continue
+		}
 
-			switch {
-			case strings.HasPrefix(f.Name.Name, GetPrefix):
-				methodType = GetPrefix
-			case strings.HasPrefix(f.Name.Name, CreatePrefix):
-				methodType = CreatePrefix
-			case strings.HasPrefix(f.Name.Name, DeletePrefix):
-				methodType = DeletePrefix
-			default:
-				continue
-			}
-
-			for typeName := range nobjectTypes {
-				if strings.HasSuffix(f.Name.Name, typeName) {
-					params, err := GetCustomRepositoryFunctionParameters(f.Type.Params)
-					if err != nil {
-						fmt.Printf("faas handler " + f.Name.Name + " custom definition replaced with default definition")
-						continue
-					}
-
-					returnParams, err := GetReturnTypesDefinition(f.Type.Results, nobjectTypes)
-					if err != nil {
-						fmt.Printf("faas handler " + f.Name.Name + " custom definition replaced with default definition")
-						continue
-					}
-
-					body, err := GetFunctionBody(set, f.Body)
-					if err != nil {
-						fmt.Printf("faas handler " + f.Name.Name + " custom definition replaced with default definition")
-						continue
-					}
-
-					isNoObjectMethodDefined[typeName][methodType] = true
-					repositoryHandlerFuncs = append(repositoryHandlerFuncs, RepositoryCustomHandlerFunc{
-						OperationName: methodType,
-						TypeName:      typeName,
-						Parameters:    params,
-						ReturnValues:  returnParams,
-						Body:          body,
-					})
+		for typeName := range nobjectTypes {
+			if strings.HasSuffix(f.Name.Name, typeName) {
+				params, err := GetCustomRepositoryFunctionParameters(f.Type.Params)
+				if err != nil {
+					fmt.Println("faas handler " + f.Name.Name + " custom definition replaced with default definition")
+					continue
 				}
-			}
 
-			_ = packageName
+				returnParams, err := GetReturnTypesDefinition(f.Type.Results, nobjectTypes)
+				if err != nil {
+					fmt.Println("faas handler " + f.Name.Name + " custom definition replaced with default definition")
+					continue
+				}
+
+				body, err := GetFunctionBody(set, f.Body)
+				if err != nil {
+					fmt.Printf("faas handler " + f.Name.Name + " custom definition replaced with default definition")
+					continue
+				}
+
+				isNoObjectMethodDefined[typeName][methodType] = true
+				repositoryHandlerFuncs = append(repositoryHandlerFuncs, RepositoryCustomHandlerFunc{
+					OperationName: methodType,
+					TypeName:      typeName,
+					Parameters:    params,
+					ReturnValues:  returnParams,
+					Body:          body,
+					Imports:       GetImports(set, detectedFunction.Imports),
+				})
+			}
 		}
 	}
 
@@ -113,118 +116,118 @@ func PrepareStateChangingHandlers(path string, moduleName string, nobjectTypes m
 	packs, err := parser.ParseDir(set, path, nil, 0)
 	AssertDirParsed(err)
 
-	funcsMap := GetPackageFunctionMap(packs)
+	functions := GetPackageFunctions(packs)
 
 	handlerFuncs := []HandlerFunc{}
-	for packageName, funcs := range funcsMap {
-		for _, f := range funcs {
-			if f.Recv == nil || f.Name.Name == GetTypeName {
+	for _, detectedFunction := range functions {
+		f := detectedFunction.Function
+		if f.Recv == nil || f.Name.Name == GetTypeName {
+			continue
+		}
+
+		ownerType := strings.TrimPrefix(types.ExprString(f.Recv.List[0].Type), "*")
+		if _, ok := nobjectTypes[ownerType]; !ok {
+			fmt.Println("Member type does not implement Nobject interface. Handler generation for " + f.Name.Name + "skipped")
+			continue
+		}
+
+		newHandler := HandlerFunc{
+			OrginalPackage:      moduleName + "/" + detectedFunction.PackageName,
+			OrginalPackageAlias: OrginalPackageAlias,
+			HandlerName:         f.Name.Name + HandlerSuffix,
+			Signature:           "func " + f.Name.Name + HandlerSuffix + HandlerParameters,
+			OwnerType:           ownerType,
+			Imports:             GetImports(set, detectedFunction.Imports),
+		}
+
+		// 4 cases:
+		// C1: no return parameters
+		// C2: 1 return: error
+		// C3: 1 return: non-error
+		// C4: 2 return: non-error, error
+		errorTypeFound := false
+		if f.Type.Results == nil {
+			// C1
+			newHandler.Signature += " error"
+		} else {
+			errorTypeFound = types.ExprString(f.Type.Results.List[len(f.Type.Results.List)-1].Type) == "error"
+			if !errorTypeFound && len(f.Type.Results.List) > 1 {
+				fmt.Println("Maximum allowed number of non-error return parameters is 1. Handler generation for " + f.Name.Name + "skipped")
 				continue
-			}
-
-			ownerType := strings.TrimPrefix(types.ExprString(f.Recv.List[0].Type), "*")
-			if _, ok := nobjectTypes[ownerType]; !ok {
-				fmt.Println("Member type does not implement Nobject interface. Handler generation for " + f.Name.Name + "skipped")
-				continue
-			}
-
-			newHandler := HandlerFunc{
-				OrginalPackage:      moduleName + "/" + packageName,
-				OrginalPackageAlias: OrginalPackageAlias,
-				HandlerName:         f.Name.Name + HandlerSuffix,
-				Signature:           "func " + f.Name.Name + HandlerSuffix + HandlerParameters,
-				OwnerType:           ownerType,
-			}
-
-			// 4 cases:
-			// C1: no return parameters
-			// C2: 1 return: error
-			// C3: 1 return: non-error
-			// C4: 2 return: non-error, error
-			errorTypeFound := false
-			if f.Type.Results == nil {
-				// C1
-				newHandler.Signature += " error"
 			} else {
-				errorTypeFound = types.ExprString(f.Type.Results.List[len(f.Type.Results.List)-1].Type) == "error"
-				if !errorTypeFound && len(f.Type.Results.List) > 1 {
-					fmt.Println("Maximum allowed number of non-error return parameters is 1. Handler generation for " + f.Name.Name + "skipped")
-					continue
+
+				newHandler.OptionalReturnType = types.ExprString(f.Type.Results.List[0].Type)
+				if _, ok := nobjectTypes[newHandler.OptionalReturnType]; ok {
+					newHandler.OptionalReturnType = newHandler.OrginalPackageAlias + "." + newHandler.OptionalReturnType
+				}
+
+				if !errorTypeFound {
+					// C3
+					newHandler.Signature += "(" + newHandler.OptionalReturnType + ", error)"
+					newHandler.ReturnFromInvocation = "result :="
+					newHandler.OptionalReturnVar = "result"
 				} else {
-
-					newHandler.OptionalReturnType = types.ExprString(f.Type.Results.List[0].Type)
-					if _, ok := nobjectTypes[newHandler.OptionalReturnType]; ok {
-						newHandler.OptionalReturnType = newHandler.OrginalPackageAlias + "." + newHandler.OptionalReturnType
-					}
-
-					if !errorTypeFound {
-						// C3
-						newHandler.Signature += "(" + newHandler.OptionalReturnType + ", error)"
-						newHandler.ReturnFromInvocation = "result :="
-						newHandler.OptionalReturnVar = "result"
+					if len(f.Type.Results.List) == 1 {
+						// C2
+						newHandler.Signature += " error"
+						newHandler.ReturnFromInvocation = "err :="
 					} else {
-						if len(f.Type.Results.List) == 1 {
-							// C2
-							newHandler.Signature += " error"
-							newHandler.ReturnFromInvocation = "err :="
-						} else {
-							// C4
-							newHandler.Signature += "(" + newHandler.OptionalReturnType + " ,error)"
-							newHandler.ReturnFromInvocation = "result, err :="
-							newHandler.OptionalReturnVar = "result"
-						}
+						// C4
+						newHandler.Signature += "(" + newHandler.OptionalReturnType + " ,error)"
+						newHandler.ReturnFromInvocation = "result, err :="
+						newHandler.OptionalReturnVar = "result"
 					}
 				}
 			}
-
-			var ownerTypeName string
-			if f.Recv.List[0].Names == nil {
-				// stateless method, instance will be created just to invoke the method
-				newHandler.Stateless = true
-				newHandler.OwnerVariableName = "typeInstance"
-				ownerTypeName = "typeInstance"
-			} else {
-				newHandler.Stateless = false
-				// stateful method, create instance to invoke the method and then save state changes
-				ownerTypeName = f.Recv.List[0].Names[0].Name
-				newHandler.OwnerVariableName = f.Recv.List[0].Names[0].Name
-			}
-
-			parameters, err := GetStateChangningFunctionParameters(f.Type.Params)
-			if err != nil {
-				fmt.Println("Maximum allowed number of parameters is 1. Handler generation for " + f.Name.Name + "skipped")
-				continue
-			}
-			newHandler.Invocation = ownerTypeName + "." + f.Name.Name + "(" + parameters + ")"
-
-			handlerFuncs = append(handlerFuncs, newHandler)
 		}
+
+		var ownerTypeName string
+		if f.Recv.List[0].Names == nil {
+			// stateless method, instance will be created just to invoke the method
+			newHandler.Stateless = true
+			newHandler.OwnerVariableName = "typeInstance"
+			ownerTypeName = "typeInstance"
+		} else {
+			newHandler.Stateless = false
+			// stateful method, create instance to invoke the method and then save state changes
+			ownerTypeName = f.Recv.List[0].Names[0].Name
+			newHandler.OwnerVariableName = f.Recv.List[0].Names[0].Name
+		}
+
+		parameters, err := GetStateChangingFunctionParameters(f.Type.Params)
+		if err != nil {
+			fmt.Println("Maximum allowed number of parameters is 1. Handler generation for " + f.Name.Name + "skipped")
+			continue
+		}
+		newHandler.Invocation = ownerTypeName + "." + f.Name.Name + "(" + parameters + ")"
+
+		handlerFuncs = append(handlerFuncs, newHandler)
 	}
 
 	return handlerFuncs
 }
 
-func GetPackageFunctionMap(packs map[string]*ast.Package) map[string][]*ast.FuncDecl {
-	funcsMap := make(map[string][]*ast.FuncDecl)
+func GetPackageFunctions(packs map[string]*ast.Package) []detectedFunction {
+	var detectedFunctions []detectedFunction
 
 	for packageName, pack := range packs {
 		for _, f := range pack.Files {
 			for _, d := range f.Decls {
 				if fn, isFn := d.(*ast.FuncDecl); isFn {
-
-					if funcsMap[packageName] == nil {
-						funcsMap[packageName] = []*ast.FuncDecl{}
-					}
-					funcsMap[packageName] = append(funcsMap[packageName], fn)
+					detectedFunctions = append(detectedFunctions, detectedFunction{
+						Function:    fn,
+						Imports:     f.Imports,
+						PackageName: packageName,
+					})
 				}
 			}
 		}
 	}
 
-	return funcsMap
+	return detectedFunctions
 }
 
-func GetStateChangningFunctionParameters(params *ast.FieldList) (string, error) {
+func GetStateChangingFunctionParameters(params *ast.FieldList) (string, error) {
 	if params.List == nil || len(params.List) == 0 {
 		return "", nil
 	} else if len(params.List) > 1 {
@@ -275,4 +278,17 @@ func GetFunctionBody(fset *token.FileSet, body *ast.BlockStmt) (string, error) {
 		return "", fmt.Errorf("error occurred when parsing the function body")
 	}
 	return buf.String(), nil
+}
+
+func GetImports(fset *token.FileSet, imports []*ast.ImportSpec) string {
+	var buf bytes.Buffer
+	for _, imp := range imports {
+		err := printer.Fprint(&buf, fset, imp)
+		buf.WriteString("\n")
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	return buf.String()
 }
