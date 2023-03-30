816d79dd48a189d434eb10421be1d425fcea01c2
Kinga Marek <marekkinga97@gmail.com>
2023-03-30 15:47:48 +0200

    evaluation/example: remove accidentally comitted client_lib in evaluation/, exclude example/client_lib from .gitignore to provide an example

---
+++ example/client_lib/reference_ctors.go
@@ -0,0 +1,64 @@
+package client_lib
+
+
+
+
+func OrderReferenceList(capacity ...int) ReferenceList[order] {
+	if capacity != nil {
+		return make(ReferenceList[order], 0, capacity[0])
+	}
+	return *new(ReferenceList[order])
+}
+ 
+
+ 
+
+
+
+func ProductReferenceList(capacity ...int) ReferenceList[product] {
+	if capacity != nil {
+		return make(ReferenceList[product], 0, capacity[0])
+	}
+	return *new(ReferenceList[product])
+}
+ 
+
+
+
+func ShippingReferenceList(capacity ...int) ReferenceList[shipping] {
+	if capacity != nil {
+		return make(ReferenceList[shipping], 0, capacity[0])
+	}
+	return *new(ReferenceList[shipping])
+}
+ 
+
+
+
+func ShopReferenceList(capacity ...int) ReferenceList[shop] {
+	if capacity != nil {
+		return make(ReferenceList[shop], 0, capacity[0])
+	}
+	return *new(ReferenceList[shop])
+}
+ 
+
+
+
+func UserReferenceList(capacity ...int) ReferenceList[user] {
+	if capacity != nil {
+		return make(ReferenceList[user], 0, capacity[0])
+	}
+	return *new(ReferenceList[user])
+}
+ 
+
+
+
+func DiscountReferenceList(capacity ...int) ReferenceList[discount] {
+	if capacity != nil {
+		return make(ReferenceList[discount], 0, capacity[0])
+	}
+	return *new(ReferenceList[discount])
+}
+ 
